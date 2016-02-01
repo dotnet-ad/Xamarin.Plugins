c66b7e48d4b29666a2dd7ac15f92e46df83da3bf
Ned Lanon <ned.lanon@outlook.com>
2016-02-01 14:11:12 +0100

    Improved navigation actions.

---
+++ UriNavigationService/UriNavigationService/UriNavigationService/UriNavigationService/Plugin.UriNavigationService.UWP/Actions/PageNavigation.cs
@@ -0,0 +1,39 @@
+﻿namespace Plugin.UriNavigationService.Actions
+{
+    using Abstractions.Actions;
+    using Abstractions.Attributes;
+    using System;
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public class PageNavigation<T> : NavigationActionBase where T : Page
+    {
+        public PageNavigation(Type context) : base(context, CrossUriNavigationService.Current)
+        {
+
+        }
+
+        public override Task Push(string uri)
+        {
+            var page = typeof(T);
+            var frame = (Frame)Window.Current.Content;
+            if(frame != null)
+            {
+                frame.Navigate(page, uri);
+            }
+            return Task.FromResult(true);
+        }
+
+        public override Task Pop()
+        {
+            var frame = (Frame)Window.Current.Content;
+            if(frame != null && frame.CanGoBack)
+            {
+                frame.GoBack();
+            }
+            return Task.FromResult(true);
+        }
+
+    }
+}
