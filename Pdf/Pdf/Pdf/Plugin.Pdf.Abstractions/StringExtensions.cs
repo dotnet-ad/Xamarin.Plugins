e291cf8896f7fda6fe070574429dae0849c628e0
Aloïs Deniel <alois.deniel@gmail.com>
2016-03-06 16:17:41 +0100

    Added iOS sample references.

--- Pdf/Pdf/Pdf/Plugin.Pdf.Abstractions/StringExtensions.cs
+++ Pdf/Pdf/Pdf/Plugin.Pdf.Abstractions/StringExtensions.cs
@@ -6,28 +6,34 @@ using System.Threading.Tasks;
 
 namespace Plugin.Pdf.Abstractions
 {
-    public static class StringExtensions
-    {
-        private const string PathSeparator = "/";
+	public static class StringExtensions
+	{
+		private const string PathSeparator = "/";
 
-        public static string ToFolderPath(this string path)
-        {
-            return PathSeparator + string.Join(PathSeparator, path.SplitPath()) + PathSeparator;
-        }
+		public static string ToFolderPath (this string path)
+		{
+			return PathSeparator + string.Join (PathSeparator, path.SplitPath ()) + PathSeparator;
+		}
 
-        public static string ToFilePath(this string path)
-        {
-            return PathSeparator + string.Join(PathSeparator, path.SplitPath());
-        }
+		public static string ToFilePath (this string path)
+		{
+			return PathSeparator + string.Join (PathSeparator, path.SplitPath ());
+		}
 
-        public static string[] SplitPath(this string path)
-        {
-            return path.Split(new char[] { '/', '\\' },StringSplitOptions.RemoveEmptyEntries);
-        }
+		public static string[] SplitPath (this string path)
+		{
+			return path.Split (new char[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+		}
 
-        public static bool IsDistantUrl(this string url)
-        {
-            return url.StartsWith("http://") || url.StartsWith("https://");
-        }
-    }
+
+		public static string AppendPath (this string path, string other)
+		{
+			return path.ToFolderPath ().TrimEnd (new char[] { '/' }) + other;
+		}
+
+		public static bool IsDistantUrl (this string url)
+		{
+			return url.StartsWith ("http://") || url.StartsWith ("https://");
+		}
+	}
 }
--- Pdf/Pdf/Pdf/Plugin.Pdf.iOS/PdfImplementation.cs
+++ Pdf/Pdf/Pdf/Plugin.Pdf.iOS/PdfImplementation.cs
@@ -6,85 +6,86 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using UIKit;
+using System.Diagnostics;
 
 namespace Plugin.Pdf
 {
-    /// <summary>
-    /// Implementation for Pdf
-    /// </summary>
-    public class PdfImplementation : IPdf
-    {
-        public PdfImplementation()
-        {
-            this.Hash = new Hash();
-        }
+	/// <summary>
+	/// Implementation for Pdf
+	/// </summary>
+	public class PdfImplementation : IPdf
+	{
+		public PdfImplementation ()
+		{
+			this.Hash = new Hash ();
+		}
 
-        private const string LocalPdfCacheDirectory = ".pdf";
+		private const string LocalPdfCacheDirectory = ".pdf";
 
-        public IHash Hash { get; set; }
+		public IHash Hash { get; set; }
 
-        private static readonly nfloat One = (nfloat)1.0;
+		private static readonly nfloat One = (nfloat)1.0;
 
-        private static UIImage RenderImage(CGPDFPage page)
-        {
-            var rect = page.GetBoxRect(CGPDFBox.Crop);
-            var pageRotation = page.RotationAngle;
-            var size = rect.Size;
+		private static UIImage RenderImage (CGPDFPage page)
+		{
+			var rect = page.GetBoxRect (CGPDFBox.Crop);
+			var pageRotation = page.RotationAngle;
+			var size = rect.Size;
 
-            UIGraphics.BeginImageContextWithOptions(size, false, (nfloat)(1));
+			UIGraphics.BeginImageContextWithOptions (size, false, (nfloat)(1));
 
-            var context = UIGraphics.GetCurrentContext();
-            context.SaveState();
-            context.TranslateCTM((nfloat)0.0, size.Height);
-            context.ScaleCTM(One, (nfloat)(-1.0));
+			var context = UIGraphics.GetCurrentContext ();
+			context.SaveState ();
+			context.TranslateCTM ((nfloat)0.0, size.Height);
+			context.ScaleCTM (One, (nfloat)(-1.0));
 
-            context.SetFillColor(One, One);
-            context.FillRect(rect);
+			context.SetFillColor (One, One);
+			context.FillRect (rect);
 
-            var transform = page.GetDrawingTransform(CGPDFBox.Crop, rect, 0, true);
-            context.ConcatCTM(transform);
-            context.DrawPDFPage(page);
+			var transform = page.GetDrawingTransform (CGPDFBox.Crop, rect, 0, true);
+			context.ConcatCTM (transform);
+			context.DrawPDFPage (page);
 
-            var image = UIGraphics.GetImageFromCurrentImageContext();
-            context.RestoreState();
-            UIGraphics.EndImageContext();
+			var image = UIGraphics.GetImageFromCurrentImageContext ();
+			context.RestoreState ();
+			UIGraphics.EndImageContext ();
 
-            return image;
-        }
+			return image;
+		}
 
-        private string[] Render(CGPDFDocument pdf, string outputDirectory)
-        {
-            var result = new string[pdf.Pages];
+		private string[] Render (CGPDFDocument pdf, string outputDirectory)
+		{
+			var result = new string[pdf.Pages];
 
-            for (int i = 0; i < pdf.Pages; i++)
-            {
-                var pagePath = string.Format("{0}/{1}.png", outputDirectory.TrimEnd(new char[] { '/', '\\' }), i);
-                var page = pdf.GetPage(i);
-                var image = RenderImage(page);
-                var data = image.AsPNG();
-                data.Save(pagePath, true);
-                result[i] = pagePath;
-            }
+			for (int i = 0; i < pdf.Pages; i++) {
+				var pagePath = string.Format ("{0}/{1}.png", outputDirectory.TrimEnd (new char[] { '/', '\\' }), i);
+				var page = pdf.GetPage (i);
+				var image = RenderImage (page);
+				var data = image.AsPNG ();
+				data.Save (pagePath, true);
+				result [i] = pagePath;
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        public Task<Abstractions.PdfDocument> Rasterize(string pdfPath, bool cachePirority = true)
-        {
-            var pdf = pdfPath.IsDistantUrl() ? CGPDFDocument.FromUrl(pdfPath) : CGPDFDocument.FromFile(pdfPath);
+		public Task<Abstractions.PdfDocument> Rasterize (string pdfPath, bool cachePirority = true)
+		{
+			var pdf = pdfPath.IsDistantUrl () ? CGPDFDocument.FromUrl (pdfPath) : CGPDFDocument.FromFile (pdfPath);
 
-            var hash = localName == null ? this.Hash.Create(pdfPath) : localName;
-            var path = (LocalPdfCacheDirectory.ToFolderPath() + hash).ToFolderPath();
-            var pagesPaths = this.Render(pdf, path);
-            return Task.FromResult(new PdfDocument()
-            {
-                Pages = pagesPaths.Select((p) => new PdfPage() { Path = p }),
-            });
-        }
+			var documents = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+			var hash = this.Hash.Create (pdfPath);
+			var path = documents.AppendPath ((LocalPdfCacheDirectory.ToFolderPath () + hash).ToFolderPath ());
+			var pagesPaths = this.Render (pdf, path);
 
-        public Task<PdfDocument> GetRasterized(string pdfPath)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			return Task.FromResult (new PdfDocument () {
+				Pages = pagesPaths.Select ((p) => new PdfPage () { Path = p }),
+			});
+		}
+
+		public Task<PdfDocument> GetRasterized (string pdfPath)
+		{
+			throw new NotImplementedException ();
+		}
+	}
 }
