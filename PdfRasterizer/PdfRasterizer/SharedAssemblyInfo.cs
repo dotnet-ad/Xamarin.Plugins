c579c805e7342a08008eb2ef7c4cec374a4d8030
Aloïs Deniel <alois.deniel@gmail.com>
2016-03-14 08:27:42 +0100

    Added missing assembly version.

--- PdfRasterizer/PdfRasterizer/SharedAssemblyInfo.cs
+++ PdfRasterizer/PdfRasterizer/SharedAssemblyInfo.cs
@@ -1,7 +1,8 @@
 ﻿using System.Resources;
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.3.4.0")]
+[assembly: AssemblyVersion("0.3.5.0")]
+[assembly: AssemblyFileVersion("0.3.5.0")]
 [assembly: AssemblyCompany("Aloïs Deniel")]
 [assembly: AssemblyProduct("Plugin.PdfRasterizer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
